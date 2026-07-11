bac39d71753e013776be2840668a38f990b2584d
saif520 <saifuddin2k17@gmail.com>
2026-07-11 21:30:23 +0530

    Rat in a Maze

--- take_u_forward/recursion/20_Rat_in_a_maze.cpp
+++ take_u_forward/recursion/20_Rat_in_a_maze.cpp
@@ -1,73 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Class to solve the Rat in a Maze problem
 class Solution {
 public:
-    // Function to check if the cell is within maze and valid to move
-    bool isSafe(int x, int y, int n, vector<vector<int>> &maze, 
+    // Check whether the cell is valid
+    bool isSafe(int x, int y, int n, vector<vector<int>> &maze,
                 vector<vector<int>> &visited) {
-        return (x >= 0 && x < n && y >= 0 && y < n && 
-                maze[x][y] == 1 && visited[x][y] == 0);
+        return (x >= 0 && x < n &&
+                y >= 0 && y < n &&
+                maze[x][y] == 1 &&
+                visited[x][y] == 0);
     }
 
-    // Function to solve maze using backtracking
-    void solve(int x, int y, int n, vector<vector<int>> &maze, 
-               vector<vector<int>> &visited, string path, 
+    // Backtracking function
+    void solve(int x, int y, int n, vector<vector<int>> &maze,
+               vector<vector<int>> &visited, string path,
                vector<string> &res) {
 
-        // If destination reached, store the path
+        // Destination reached
         if (x == n - 1 && y == n - 1) {
             res.push_back(path);
             return;
         }
 
-        // Mark the cell visited
+        // Mark current cell as visited
         visited[x][y] = 1;
 
-        // Try moving Down
-        if (isSafe(x + 1, y, n, maze, visited)) {
+        // Down
+        if (isSafe(x + 1, y, n, maze, visited))
             solve(x + 1, y, n, maze, visited, path + "D", res);
-        }
-        // Try moving Left
-        if (isSafe(x, y - 1, n, maze, visited)) {
-            solve(x, y - 1, n, maze, visited, path + "L", res);
-        }
-        // Try moving Right
-        if (isSafe(x, y + 1, n, maze, visited)) {
-            solve(x, y + 1, n, maze, visited, path + "R", res);
-        }
-        // Try moving Up
-        if (isSafe(x - 1, y, n, maze, visited)) {
-            solve(x - 1, y, n, maze, visited, path + "U", res);
-        }
 
-        // Backtrack: unmark cell as visited
+        // Left
+        if (isSafe(x, y - 1, n, maze, visited))
+            solve(x, y - 1, n, maze, visited, path + "L", res);
+
+        // Right
+        if (isSafe(x, y + 1, n, maze, visited))
+            solve(x, y + 1, n, maze, visited, path + "R", res);
+
+        // Up
+        if (isSafe(x - 1, y, n, maze, visited))
+            solve(x - 1, y, n, maze, visited, path + "U", res);
+
+        // Backtrack
         visited[x][y] = 0;
     }
 
-    // Main function to find all paths
-    vector<string> findPath(vector<vector<int>> &maze, int n) {
+    // Function expected by the driver
+    vector<string> ratInMaze(vector<vector<int>> &maze) {
+        int n = maze.size();
         vector<string> res;
+
+        if (n == 0 || maze[0][0] == 0)
+            return res;
+
         vector<vector<int>> visited(n, vector<int>(n, 0));
-        if (maze[0][0] == 1) {
-            solve(0, 0, n, maze, visited, "", res);
-        }
+
+        solve(0, 0, n, maze, visited, "", res);
+
+        sort(res.begin(), res.end()); // Lexicographical order
         return res;
     }
 };
-
-// Driver code
-int main() {
-    vector<vector<int>> maze = {
-        {1, 0, 0, 0},
-        {1, 1, 0, 1},
-        {1, 1, 0, 0},
-        {0, 1, 1, 1}
-    };
-    int n = maze.size();
-    Solution obj;
-    vector<string> paths = obj.findPath(maze, n);
-
-    for (auto &p : paths) cout << p << " ";
-}
